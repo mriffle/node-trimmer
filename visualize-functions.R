21e805741c956658be837fc644a1fa6d2915f70c
Michael Riffle <mriffle@uw.edu>
2023-02-07 20:23:29 -0800

    visualization work

--- visualize-functions.R
+++ visualize-functions.R
@@ -7,7 +7,8 @@ visualize_go_dag = function(
     parents
 ) {
   levels = get_levels(go_accs, children, parents)
-  nodes = data.frame(id = go_accs, label = levels, levels = levels)
+  print(levels)
+  nodes = data.frame(id = go_accs, label = go_accs, level = levels)
   edges = data.frame(from = children, to = parents)
   
   visNetwork(nodes, edges, width = "100%", height="500px") %>% 
@@ -20,20 +21,17 @@ get_levels = function(go_accs, children, parents) {
   print(parent_child_map[['all']])
   level = 1
   levels = rep(c(0),times=length(go_accs))
-  root_nodes = get_roots(children, parents)
-
-  fill_levels(levels, level, go_accs, root_nodes, parent_child_map)
-  return(levels)
-}
-
-fill_levels = function(levels, level, go_accs, go_accs_to_fill, parent_child_map) {
-  levels[which(go_accs %in% go_accs_to_fill)] = level
-
-  level = level + 1
-  for(go_acc in go_accs_to_fill) {
-    new_nodes = parent_child_map[[go_acc]]
-    if(!is.null(new_nodes) && length(new_nodes) > 0) {
-      fill_levels(levels, level, go_acc, new_nodes, parent_child_map)
+  nodes_to_fill = get_roots(children, parents)
+  
+  while(!is.null(nodes_to_fill) && length(nodes_to_fill) > 0) {
+    levels[which(go_accs %in% nodes_to_fill)] = level
+    new_nodes_to_fill = c()
+    level = level + 1
+    for(go_acc in nodes_to_fill) {
+      new_nodes_to_fill = append(new_nodes_to_fill, parent_child_map[[go_acc]])
     }
+    nodes_to_fill = unique(new_nodes_to_fill)
   }
+
+  return(levels)
 }
